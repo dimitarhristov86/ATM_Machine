374a13d22037168c56aaf4fa07a2387cf1744596
dimitar_hristov86 <dimitar_hristov86@abv.bg>
2021-06-01 10:51:13 +0300

    ATM_Machine

--- backend.py
+++ backend.py
@@ -15,19 +15,18 @@ def main_menu():
         client = CLIENTS.get(name_input)
         if pin_validation(client):
             balance = client["balance"]
-            print(f"Your balance is {balance}")
+            print(f"Your balance is {balance:.2f}")
             action = input("Please chose an action using code reference(1,2,3)"
                            "\n1.Withdraw\n2.Deposit\n3.View balance\n-->")
             if action == "1":
                 withdraw_amount = get_withdraw(balance)
                 new_balance = balance - withdraw_amount
-                CLIENTS[f"{client}"][f"{balance}"] = [f"{new_balance}"]
-                print(f"You just withdraw {withdraw_amount}lv.\nYour new balance is {new_balance}lv")
+                print(f"You just withdraw {withdraw_amount:.2f}lv.\nYour new balance is {new_balance:.2f}lv")
             elif action == "2":
                 deposit_amount = get_deposit(balance)
                 new_balance = balance + deposit_amount
-                print(f"You just deposit {deposit_amount}lv\nYour new balance is {new_balance}lv")
+                print(f"You just deposit {deposit_amount:.2f}lv\nYour new balance is {new_balance:.2f}lv")
             elif action == "3":
-                print(f"Your balance is {balance}lv")
+                print(f"Your balance is {balance:.2f}lv")
     else:
         print(f"No such client {name_input}")
--- clients.py
+++ clients.py
@@ -1,14 +1,15 @@
 CLIENTS = {
     "dimitar": {
         "pin": "1234",
-        "balance": 1500.98
+        "balance": float(1500.56)
     },
     "sonya": {
         "pin": "6666",
-        "balance": 2520.65
+        "balance": float(2520.65)
     },
     "vladimir": {
         "pin": "9876",
-        "balance": 4523.78
+        "balance": float(4523.63)
+
     }
 }
